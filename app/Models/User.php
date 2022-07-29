e98e9af02ada2c8f991c3aa7bb2d228bb7f2111f
Kristian Velazquez <kristian.velazquez25.schuler@gmail.com>
2022-07-29 08:36:20 -0500

    Notifications

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,6 +10,8 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    use Notifiable;
+
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
@@ -17,11 +19,6 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -41,4 +38,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setPasswordAttribute($password) 
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
+
+    public function routeNotificationForVonage($notification)
+    {
+        return $this->phone_number;
+    }
 }

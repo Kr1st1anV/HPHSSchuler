50f6818eed5bea52b7bd97bae55d94190c22ba53
Kristian Velazquez <kristian.velazquez25.schuler@gmail.com>
2022-07-29 17:07:23 -0500

    posts

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -2,10 +2,10 @@
 
 <div class="box">
     <div class="image">
-        <!--/Users/kristianv/HPHSSchulerApp/storage/app/public-->
     <img src="{{ asset('storage/' . $post->thumbnail) }}" alt="image">
     </div>
     <div class="content">
+        <p>{{ $post->thumbnail }}</p>
         <h3>{{ $post->title }}</h3>
         <p>{!! $post->body !!}</p>
     </div>

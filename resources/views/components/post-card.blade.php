db78e10d8d1f9704b5af8972b98f5d830c43ecfa
Kristian Velazquez <kristian.velazquez25.schuler@gmail.com>
2022-07-29 17:30:21 -0500

    Posts

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -5,7 +5,6 @@
     <img src="{{ asset('storage/' . $post->thumbnail) }}" alt="image">
     </div>
     <div class="content">
-        <h1>{{ asset('storage/' . $post->thumbnail) }}</h1>
         <h3>{{ $post->title }}</h3>
         <p>{!! $post->body !!}</p>
     </div>

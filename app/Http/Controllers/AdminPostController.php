adec3841eecd29a7a2a72963c1215d567871b58c
Kristian Velazquez <kristian.velazquez25.schuler@gmail.com>
2022-07-28 21:12:45 -0500

    Admin

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -24,7 +24,6 @@ class AdminPostController extends Controller
     public function store() 
     {
         Post::create(array_merge($this->validatePost(), [
-            'user_id' => auth()->id(),
             'thumbnail' => request()->file('thumbnail')->store('thumbnails')
         ]));
 

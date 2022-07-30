ce7ab76a20df05b93ac24f762c841a2c0e8f7e53
Kristian Velazquez <kristian.velazquez25.schuler@gmail.com>
2022-07-30 16:39:53 -0500

    js

--- public/js/app.js
+++ public/js/app.js
@@ -38,9 +38,9 @@ var swiper = new Swiper(".reviews-slider", {
 });
 
 let loadMoreBtn = document.querySelector('.packages .load-more .btn');
-if (loadMoreBtn !== null) {
-let currentItem = 6;
-loadMoreBtn.onclick = () =>{
+   if (loadMoreBtn !== null) {
+   let currentItem = 6;
+   loadMoreBtn.onclick = () =>{
    let boxes = [...document.querySelectorAll('.packages .box-container .box')]; 
    for (var i = currentItem; i <= currentItem + 3; i++){
       if(i >= boxes.length){
@@ -48,5 +48,5 @@ loadMoreBtn.onclick = () =>{
       }
       boxes[i].style.display = 'inline-block';
    };
-}
+   }
 }

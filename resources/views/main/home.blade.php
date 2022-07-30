a9581a7e1dfccda9a3380d2385fb91fbdde5cee2
Kristian Velazquez <kristian.velazquez25.schuler@gmail.com>
2022-07-30 16:24:41 -0500

    Service worker

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -40,12 +40,4 @@
 </section>
 
 </section>
-<script src="{{ asset('/sw.js') }}"></script>
-<script>
-    if (!navigator.serviceWorker.controller) {
-        navigator.serviceWorker.register("/sw.js").then(function (reg) {
-            console.log("Service worker has been registered for scope: " + reg.scope);
-        });
-    }
-</script>
 </x-layout>

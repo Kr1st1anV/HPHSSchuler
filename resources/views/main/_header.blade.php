91756529b6ede928ebefa797a72f4b5fcf765352
Kristian Velazquez <kristian.velazquez25.schuler@gmail.com>
2022-07-31 16:32:24 -0500

    header

--- resources/views/main/_header.blade.php
+++ resources/views/main/_header.blade.php
@@ -7,24 +7,24 @@
          <div class="swiper-slide slide" style="background:url(images/banner.jpeg) no-repeat">
             <div class="content">
                <span>HPHS Schuler Scholars</span>
-               <h3>Meet new people</h3>
-               <a href="/posts" class="btn">Participate</a>
+               <h3>A new journey</h3>
+               <a href="/posts" class="btn">Explore</a>
             </div>
          </div>
 
          <div class="swiper-slide slide" style="background:url(images/new_places.jpeg) no-repeat">
             <div class="content">
                <span>HPHS Schuler Scholars</span>
-               <h3>discover new places</h3>
-               <a href="/posts" class="btn">Explore</a>
+               <h3>Make new friends</h3>
+               <a href="/posts" class="btn">Communicate</a>
             </div>
          </div>
 
          <div class="swiper-slide slide" style="background:url(images/block3.png) no-repeat">
             <div class="content">
                <span>HPHS Schuler Scholars</span>
-               <h3>Take on Risks</h3>
-               <a href="/posts" class="btn">Stay Updated</a>
+               <h3>Learn, Dream, Achieve</h3>
+               <a href="/posts" class="btn">Feel Accomplished</a>
             </div>
          </div>
          

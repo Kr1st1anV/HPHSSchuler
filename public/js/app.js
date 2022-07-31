571e19cedce151c6684384ff0d6ba153f18ed09c
Kristian Velazquez <kristian.velazquez25.schuler@gmail.com>
2022-07-31 12:57:14 -0500

    CSS

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,6 @@
 window.onload = function () {
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .navbar');
-let header = document.querySelector('.header-section-2');
-
-if (document.documentElement.style.maxWidth < "768px") {
-   header.remove();
-}
 
 menu.onclick = () =>{
    menu.classList.toggle('fa-times');

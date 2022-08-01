bbe61b7d86b67dd6c04a09803cc37bb650f2720a
Kristian Velazquez <kristian.velazquez25.schuler@gmail.com>
2022-08-01 15:09:32 -0500

    PWA Done

--- resources/views/es/register/create.blade.php
+++ resources/views/es/register/create.blade.php
@@ -10,7 +10,7 @@
         <x-es.form.number name="phone_number" type="tel" label="número de teléfono"/>
         <x-es.form.input name="password" type="password" autocomplete="new-password" label="contraseña"/>
         <x-form.button>Enviar</x-form.button>
-        <p>Ya tienes una accounta? <a href="/es/login">Ingresa</a></p>
+        <p>Ya tienes una cuenta? <a href="/es/login">Ingresa</a></p>
     </form>
 </div>
 </x-es.layout>
--- resources/views/es/sessions/create.blade.php
+++ resources/views/es/sessions/create.blade.php
@@ -7,7 +7,7 @@
         <x-form.input name="email" type="email" autocomplete="username"/>
         <x-es.form.input name="password" type="password" autocomplete="new-password" label="contraseña"/>
         <x-form.button>Ingresar</x-form.button>
-        <p>No tienes una accuenta? <a href="/es/register">Registro</a></p>
+        <p>No tienes una cuenta? <a href="/es/register">Registro</a></p>
     </form>
 </div>
 </x-es.layout>

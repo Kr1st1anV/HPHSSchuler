7f595229bac818e874e45b96b40b14025efb0550
Kristian Velazquez <kristian.velazquez25.schuler@gmail.com>
2022-08-01 15:46:48 -0500

    PWA

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -74,7 +74,7 @@ return [
                 'purpose' => 'any'
             ],
             '512x512' => [
-                'path' => '/images/icons/schuler.png',
+                'path' => '/images/icons/hphs-schuler-512x512.png',
                 'purpose' => 'any'
             ],
         ],

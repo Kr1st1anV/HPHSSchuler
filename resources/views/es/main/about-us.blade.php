070b10677789a0aae54b8fc7a1de472ad160b430
Kristian Velazquez <kristian.velazquez25.schuler@gmail.com>
2022-08-16 18:46:19 -0500

    About us Spanish

--- resources/views/es/main/about-us.blade.php
+++ resources/views/es/main/about-us.blade.php
@@ -40,21 +40,21 @@
     <div class="swiper reviews-slider">
       <div class="swiper-wrapper">
         <div class="swiper-slide slide">
-          <p>After graduating from Agnes Scott College, Cheryl moved to Ecuador and was an English Professor at the Universidad Estatal de Bolívar in the small Andean town of San Miguel de Bolívar. After four years in Ecuador, teaching and building the Universidad's language program and curriculum, she returned to the United States to pursue her graduate degree. At Vanderbilt University, she studied education policy for K-12 language learners and received her Master's in Public Policy. Cheryl came to Schuler in 2017 as an Education Counselor.<br><br>Cheryl enjoys baking, doing yoga, and watching old movies.</p>
+          <p>Después de graduarse de Agnes Scott College, Cheryl se mudó a Ecuador y fue profesora de inglés en la Universidad Estatal de Bolívar en el pequeño pueblo andino de San Miguel de Bolívar. Después de cuatro años en Ecuador, enseñando y construyendo el programa y plan de estudios de idiomas de la Universidad, regresó a los Estados Unidos para obtener su título de posgrado. En la Universidad de Vanderbilt, estudió políticas educativas para estudiantes de idiomas K-12 y recibió su Maestría en Políticas Públicas. Cheryl llegó a Schuler en 2017 como consejera de educación.<br><br>A Cheryl le gusta hornear, hacer yoga y ver películas antiguas.</p>
           <h3>Cheryl Smith</h3>
           <span>Staff</span>
           <img src="../images/Cheryl_Smith.jpeg">
           <span><a href="mailto:cheryls@schulerprogram.org" class="mail-link">cheryls@schulerprogram.org</a><br>(817) 909-0215</span>
         </div>
         <div class="swiper-slide slide">
-          <p>My name is Danielle Brockstein and I am one of the Scholar Coaches here at Highland Park High School. I am originally from Deerfield, IL and attended Penn State University where I studied Human Development and Family Studies and Child Maltreatment Advocacy. I spent some time after college living in Denver, CO assistant teaching 9th grade math through a program called City Year, and then returned home to Highland Park to begin working with the Schuler Scholar Program. <br><br> I'm super excited to be back again this year with our amazing Scholars!</p>
+          <p>Mi nombre es Danielle Brockstein y soy una de las entrenadoras académicas aquí en Highland Park High School. Soy originario de Deerfield, IL y asistí a la Universidad de Penn State, donde estudié Desarrollo Humano y Estudios Familiares y Defensa del Maltrato Infantil. Después de la universidad, pasé un tiempo viviendo en Denver, CO, como asistente de enseñanza de matemáticas de noveno grado a través de un programa llamado City Year, y luego regresé a mi hogar en Highland Park para comenzar a trabajar con el Programa Schuler Scholar. <br><br> ¡Estoy muy emocionada de volver este año con nuestros increíbles becarios!</p>
           <h3>Danielle Brockstein</h3>
           <span>Staff</span>
           <img src="../images/Danielle Brockstein.jpeg">
           <span><a href="mailto:danielleb@schulerprogram.org" class="mail-link">danielleb@schulerprogram.org</a><br>(847) 323-9965</span>
         </div>
         <div class="swiper-slide slide">
-          <p>Dace Eaton is the STEM Coach for the Schuler Scholar Program at Highland Park High School. He went to college at the University of Chicago where he graduated in 2021 with a major in chemistry and a minor in film studies. Dace is always excited to help any scholar with their math or science questions, and wants STEM content to be as fun and interesting as possible. Dace also creates the weekly thought problem in the Schuler room and writes the daily trivia. <br><br> Some of his hobbies outside of Schuler include tap dancing and playing Magic: The Gathering, though not usually at the same time. </p>
+          <p>Dace Eaton es el Entrenador de STEM para el Programa Escolar Schuler en la Escuela Secundaria Highland Park. Fue a la universidad en la Universidad de Chicago, donde se graduó en 2021 con una especialización en química y una especialización en estudios cinematográficos. Dace siempre está emocionado de ayudar a cualquier estudiante con sus preguntas de matemáticas o ciencias, y quiere que el contenido de STEM sea lo más divertido e interesante posible. Dace también crea el problema mental semanal en la sala Schuler y escribe las trivias diarias. <br><br> Algunas de sus aficiones fuera de Schuler incluyen bailar claqué y jugar a Magic: The Gathering, aunque normalmente no al mismo tiempo.</p>
           <h3>Dace Eaton</h3>
           <span>Staff</span>
           <img src="../images/Dace Eaton.jpeg">
@@ -82,7 +82,7 @@
           <span><a href="mailto:noelle@schulerprogram.org" class="mail-link">noelle@schulerprogram.org</a><br>(224) 814-2357</span>
         </div>
         <div class="swiper-slide slide">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum iusto reiciendis consequuntur dicta laboriosam quod ducimus nostrum totam cumque minus error, possimus molestiae aut amet distinctio recusandae doloremque accusantium doloribus expedita ea corporis nam tenetur dignissimos! Facilis commodi nisi quia ratione corrupti officia fuga maiores velit maxime modi. Cum nisi aliquid deserunt, quaerat expedita laudantium quisquam corporis perferendis consequatur repellendus!</p>
+          <p>Belle (ella/ella) es consejera universitaria en Highland Park. Ella misma era estudiante en HPHS y ha sido muy divertido para ella volver a trabajar en la comunidad en la que creció. Belle se graduó de la Universidad del Norte de Illinois, donde estudió estudios de ONG y organizaciones sin fines de lucro, así como ciencias políticas y francés. <br><br> A Belle le encantan los gatos, y una de sus actividades favoritas antes del covid-19 era ser voluntaria en el refugio de animales de su localidad, facilitando adopciones y recaudando fondos.</p>
           <h3>Isabella Diaz</h3>
           <span>Staff</span>
           <img src="../images/Isabelle Diaz 2.jpeg">

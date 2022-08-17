31597836e000f36307fd5e21b80799e6933b4ce2
Kristian Velazquez <kristian.velazquez25.schuler@gmail.com>
2022-08-17 17:27:12 -0500

    About Us Sp

--- resources/views/es/main/about-us.blade.php
+++ resources/views/es/main/about-us.blade.php
@@ -40,49 +40,49 @@
     <div class="swiper reviews-slider">
       <div class="swiper-wrapper">
         <div class="swiper-slide slide">
-          <p>Después de graduarse de Agnes Scott College, Cheryl se mudó a Ecuador y fue profesora de inglés en la Universidad Estatal de Bolívar en el pequeño pueblo andino de San Miguel de Bolívar. Después de cuatro años en Ecuador, enseñando y construyendo el programa y plan de estudios de idiomas de la Universidad, regresó a los Estados Unidos para obtener su título de posgrado. En la Universidad de Vanderbilt, estudió políticas educativas para estudiantes de idiomas K-12 y recibió su Maestría en Políticas Públicas. Cheryl llegó a Schuler en 2017 como consejera de educación.<br><br>A Cheryl le gusta hornear, hacer yoga y ver películas antiguas.</p>
+          <p>Después de graduarse de Agnes Scott College, Cheryl (Ella) se mudó a Ecuador y fue profesora de inglés en la Universidad Estatal de Bolívar en el pequeño pueblo andino de San Miguel de Bolívar. Después de cuatro años en Ecuador, enseñando y construyendo el programa y plan de estudios de idiomas de la Universidad, regresó a los Estados Unidos para obtener su título de posgrado. En la Universidad de Vanderbilt, estudió políticas educativas para estudiantes de idiomas K-12 y recibió su Maestría en Políticas Públicas. Cheryl llegó a Schuler en 2017 como consejera de educación.<br><br>A Cheryl le gusta hornear, hacer yoga y ver películas antiguas.</p>
           <h3>Cheryl Smith</h3>
           <span>Staff</span>
           <img src="../images/Cheryl_Smith.jpeg">
           <span><a href="mailto:cheryls@schulerprogram.org" class="mail-link">cheryls@schulerprogram.org</a><br>(817) 909-0215</span>
         </div>
         <div class="swiper-slide slide">
-          <p>Mi nombre es Danielle Brockstein y soy una de las entrenadoras académicas aquí en Highland Park High School. Soy originario de Deerfield, IL y asistí a la Universidad de Penn State, donde estudié Desarrollo Humano y Estudios Familiares y Defensa del Maltrato Infantil. Después de la universidad, pasé un tiempo viviendo en Denver, CO, como asistente de enseñanza de matemáticas de noveno grado a través de un programa llamado City Year, y luego regresé a mi hogar en Highland Park para comenzar a trabajar con el Programa Schuler Scholar. <br><br> ¡Estoy muy emocionada de volver este año con nuestros increíbles becarios!</p>
+          <p>Mi nombre es Danielle Brockstein (Ella) y soy una de las entrenadoras académicas aquí en Highland Park High School. Soy originario de Deerfield, IL y asistí a la Universidad de Penn State, donde estudié Desarrollo Humano y Estudios Familiares y Defensa del Maltrato Infantil. Después de la universidad, pasé un tiempo viviendo en Denver, CO, como asistente de enseñanza de matemáticas de noveno grado a través de un programa llamado City Year, y luego regresé a mi hogar en Highland Park para comenzar a trabajar con el Programa Schuler Scholar. <br><br> ¡Estoy muy emocionada de volver este año con nuestros increíbles becarios!</p>
           <h3>Danielle Brockstein</h3>
           <span>Staff</span>
           <img src="../images/Danielle Brockstein.jpeg">
           <span><a href="mailto:danielleb@schulerprogram.org" class="mail-link">danielleb@schulerprogram.org</a><br>(847) 323-9965</span>
         </div>
         <div class="swiper-slide slide">
-          <p>Dace Eaton es el Entrenador de STEM para el Programa Escolar Schuler en la Escuela Secundaria Highland Park. Fue a la universidad en la Universidad de Chicago, donde se graduó en 2021 con una especialización en química y una especialización en estudios cinematográficos. Dace siempre está emocionado de ayudar a cualquier estudiante con sus preguntas de matemáticas o ciencias, y quiere que el contenido de STEM sea lo más divertido e interesante posible. Dace también crea el problema mental semanal en la sala Schuler y escribe las trivias diarias. <br><br> Algunas de sus aficiones fuera de Schuler incluyen bailar claqué y jugar a Magic: The Gathering, aunque normalmente no al mismo tiempo.</p>
+          <p>Dace Eaton (El) es el Entrenador de STEM para el Programa Escolar Schuler en la Escuela Secundaria Highland Park. Fue a la universidad en la Universidad de Chicago, donde se graduó en 2021 con una especialización en química y una especialización en estudios cinematográficos. Dace siempre está emocionado de ayudar a cualquier estudiante con sus preguntas de matemáticas o ciencias, y quiere que el contenido de STEM sea lo más divertido e interesante posible. Dace también crea el problema mental semanal en la sala Schuler y escribe las trivias diarias. <br><br> Algunas de sus aficiones fuera de Schuler incluyen bailar claqué y jugar a Magic: The Gathering, aunque normalmente no al mismo tiempo.</p>
           <h3>Dace Eaton</h3>
           <span>Staff</span>
           <img src="../images/Dace Eaton.jpeg">
           <span><a href="mailto:dacee@schulerprogram.org" class="mail-link">dacee@schulerprogram.org</a><br>(802) 999-4932</span>
         </div>
         <div class="swiper-slide slide">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, amet cumque non ratione illo perspiciatis dicta asperiores qui repellendus quibusdam minus nemo rerum, dignissimos laudantium quis laborum nobis officia repudiandae esse. Neque cum corrupti voluptas ratione doloribus aliquam veritatis, qui provident enim accusantium? Veniam natus, perspiciatis veritatis accusamus odio accusantium neque labore iure minus commodi doloremque amet distinctio aperiam vitae?</p>
-          <h3>Ruben Barajas</h3>
+          <p>Rubén A. Barajas Ruíz (El) es el Entrenador Académico Asociado en Highland Park High School. Rubén se graduó de Davidson College en 2018 con una licenciatura en Ciencias Políticas y una especialización en Estudios Latinoamericanos. A Rubén le gustan los deportes, especialmente el fútbol. Realmente disfruta pasar tiempo con su familia y amigos, y comprar Jordan aquí y allá. La experiencia más memorable de Rubén fue estudiar en Perú y visitar tantas partes diferentes del país, incluida una de las 7 maravillas del mundo, Machu Picchu. Algunos datos divertidos sobre Rubén es que estudió ruso durante dos años en la escuela secundaria. Conoció a la jueza de la Corte Suprema Sonia Sotomayor. <br><br> Además, mientras estudiaba en el extranjero en Perú, vio a Malia Obama en la frontera Perú-Bolivia. </p>
+          <h3>Rubén A. Barajas Ruíz</h3>
           <span>Staff</span>
-          <img src="../images/Ruben Barajas_87.jpeg">
+          <img src="images/Ruben Barajas_87.jpeg">
           <span><a href="mailto:rubenr@schulerprogram.org" class="mail-link">rubenr@schulerprogram.org</a><br>(773) 526-9080</span>
         </div>
         <div class="swiper-slide slide">
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea atque officiis vero temporibus? Vitae, recusandae tenetur. Vel quaerat maiores dolores rem, obcaecati non, voluptas animi quidem deleniti accusamus nihil doloribus nesciunt. Deleniti, totam commodi ea modi recusandae praesentium assumenda doloribus, impedit amet placeat sunt vero labore asperiores suscipit fugit provident sapiente ipsum beatae accusantium corrupti mollitia enim maxime et cupiditate? Nemo eos laborum asperiores eligendi!</p>
+          <p>Maeve Creagan (Ella) ha sido Entrenadora Académica Asociada en Highland Park durante cuatro años. Asistió a Hobart y William Smith College donde obtuvo una licenciatura en Ciencias Políticas y otra en Historia Americana. Después de trabajar en el Club de Niños y Niñas de Ginebra mientras dirigía grupos de servicio, Maeve aceptó un puesto en el Programa Schuler Americorps. Después de sus dos años de servicio, Maeve se convirtió en miembro de pleno derecho del personal de Schuler. <br><br> Maeve tiene una perra llamada Willa, pasan mucho tiempo juntos afuera explorando todo lo que la naturaleza tiene para ofrecer.</p>
           <h3>Maeve Creagan</h3>
           <span>Staff</span>
-          <img src="../images/Maeve Creagan.jpeg">
+          <img src="images/Maeve Creagan.jpeg">
           <span><a href="mailto:maevec@schulerprogram.org" class="mail-link">maevec@schulerprogram.org</a><br>(607) 351-7538</span>
         </div>
         <div class="swiper-slide slide">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum autem dolorem consequuntur, quo nisi reiciendis iusto culpa facilis, recusandae, voluptatibus architecto accusamus totam similique eius ab dolore. Voluptatem necessitatibus cum a praesentium suscipit, mollitia porro, obcaecati illum fugiat quam incidunt rem laboriosam illo qui quae reiciendis numquam exercitationem delectus? Ad ut tempora voluptas sed, unde consectetur totam nihil. Ratione quo, ducimus expedita saepe placeat nemo.</p>
+          <p>Noelle Dela Cruz (Ella) es la Consejera Educativa del Programa Schuler en la Escuela Secundaria Highland Park. Actualmente está cursando su Maestría en Educación en Educación Superior Urbana. En su tiempo libre le encanta viajar e ir a conciertos. Está emocionada de continuar ayudando a los becarios a lograr el éxito académico.</p>
           <h3>Noelle Dela Cruz</h3>
           <span>Staff</span>
-          <img src="../images/Noelle Dela Cruz.jpeg">
+          <img src="images/Noelle Dela Cruz.jpeg">
           <span><a href="mailto:noelle@schulerprogram.org" class="mail-link">noelle@schulerprogram.org</a><br>(224) 814-2357</span>
         </div>
         <div class="swiper-slide slide">
-          <p>Belle (ella) es consejera universitaria en Highland Park. Ella misma era estudiante en HPHS y ha sido muy divertido para ella volver a trabajar en la comunidad en la que creció. Belle se graduó de la Universidad del Norte de Illinois, donde estudió estudios de ONG y organizaciones sin fines de lucro, así como ciencias políticas y francés. <br><br> A Belle le encantan los gatos, y una de sus actividades favoritas antes del covid-19 era ser voluntaria en el refugio de animales de su localidad, facilitando adopciones y recaudando fondos.</p>
+          <p>Belle (Ella) es consejera universitaria en Highland Park. Ella misma era estudiante en HPHS y ha sido muy divertido para ella volver a trabajar en la comunidad en la que creció. Belle se graduó de la Universidad del Norte de Illinois, donde estudió estudios de ONG y organizaciones sin fines de lucro, así como ciencias políticas y francés. <br><br> A Belle le encantan los gatos, y una de sus actividades favoritas antes del covid-19 era ser voluntaria en el refugio de animales de su localidad, facilitando adopciones y recaudando fondos.</p>
           <h3>Isabella Diaz</h3>
           <span>Staff</span>
           <img src="../images/Isabelle Diaz 2.jpeg">

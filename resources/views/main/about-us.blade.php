0cd340341081080bf5d30e5020b94e38e7abfeea
Kristian Velazquez <kristian.velazquez25.schuler@gmail.com>
2022-08-17 17:21:09 -0500

    About us En

--- resources/views/main/about-us.blade.php
+++ resources/views/main/about-us.blade.php
@@ -40,49 +40,49 @@
     <div class="swiper reviews-slider">
       <div class="swiper-wrapper">
         <div class="swiper-slide slide">
-          <p>After graduating from Agnes Scott College, Cheryl moved to Ecuador and was an English Professor at the Universidad Estatal de Bolívar in the small Andean town of San Miguel de Bolívar. After four years in Ecuador, teaching and building the Universidad's language program and curriculum, she returned to the United States to pursue her graduate degree. At Vanderbilt University, she studied education policy for K-12 language learners and received her Master's in Public Policy. Cheryl came to Schuler in 2017 as an Education Counselor.<br><br>Cheryl enjoys baking, doing yoga, and watching old movies.</p>
+          <p>After graduating from Agnes Scott College, Cheryl (She/Her) moved to Ecuador and was an English Professor at the Universidad Estatal de Bolívar in the small Andean town of San Miguel de Bolívar. After four years in Ecuador, teaching and building the Universidad's language program and curriculum, she returned to the United States to pursue her graduate degree. At Vanderbilt University, she studied education policy for K-12 language learners and received her Master's in Public Policy. Cheryl came to Schuler in 2017 as an Education Counselor.<br><br>Cheryl enjoys baking, doing yoga, and watching old movies.</p>
           <h3>Cheryl Smith</h3>
           <span>Staff</span>
           <img src="images/Cheryl_Smith.jpeg">
           <span><a href="mailto:cheryls@schulerprogram.org" class="mail-link">cheryls@schulerprogram.org</a><br>(817) 909-0215</span>
         </div>
         <div class="swiper-slide slide">
-          <p>My name is Danielle Brockstein and I am one of the Scholar Coaches here at Highland Park High School. I am originally from Deerfield, IL and attended Penn State University where I studied Human Development and Family Studies and Child Maltreatment Advocacy. I spent some time after college living in Denver, CO assistant teaching 9th grade math through a program called City Year, and then returned home to Highland Park to begin working with the Schuler Scholar Program. <br><br> I'm super excited to be back again this year with our amazing Scholars!</p>
+          <p>My name is Danielle Brockstein (She/Her) and I am one of the Scholar Coaches here at Highland Park High School. I am originally from Deerfield, IL and attended Penn State University where I studied Human Development and Family Studies and Child Maltreatment Advocacy. I spent some time after college living in Denver, CO assistant teaching 9th grade math through a program called City Year, and then returned home to Highland Park to begin working with the Schuler Scholar Program. <br><br> I'm super excited to be back again this year with our amazing Scholars!</p>
           <h3>Danielle Brockstein</h3>
           <span>Staff</span>
           <img src="images/Danielle Brockstein.jpeg">
           <span><a href="mailto:danielleb@schulerprogram.org" class="mail-link">danielleb@schulerprogram.org</a><br>(847) 323-9965</span>
         </div>
         <div class="swiper-slide slide">
-          <p>Dace Eaton is the STEM Coach for the Schuler Scholar Program at Highland Park High School. He went to college at the University of Chicago where he graduated in 2021 with a major in chemistry and a minor in film studies. Dace is always excited to help any scholar with their math or science questions, and wants STEM content to be as fun and interesting as possible. Dace also creates the weekly thought problem in the Schuler room and writes the daily trivia. <br><br> Some of his hobbies outside of Schuler include tap dancing and playing Magic: The Gathering, though not usually at the same time.</p>
+          <p>Dace Eaton (He/Him) is the STEM Coach for the Schuler Scholar Program at Highland Park High School. He went to college at the University of Chicago where he graduated in 2021 with a major in chemistry and a minor in film studies. Dace is always excited to help any scholar with their math or science questions, and wants STEM content to be as fun and interesting as possible. Dace also creates the weekly thought problem in the Schuler room and writes the daily trivia. <br><br> Some of his hobbies outside of Schuler include tap dancing and playing Magic: The Gathering, though not usually at the same time.</p>
           <h3>Dace Eaton</h3>
           <span>Staff</span>
           <img src="images/Dace Eaton.jpeg">
           <span><a href="mailto:dacee@schulerprogram.org" class="mail-link">dacee@schulerprogram.org</a><br>(802) 999-4932</span>
         </div>
         <div class="swiper-slide slide">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, amet cumque non ratione illo perspiciatis dicta asperiores qui repellendus quibusdam minus nemo rerum, dignissimos laudantium quis laborum nobis officia repudiandae esse. Neque cum corrupti voluptas ratione doloribus aliquam veritatis, qui provident enim accusantium? Veniam natus, perspiciatis veritatis accusamus odio accusantium neque labore iure minus commodi doloremque amet distinctio aperiam vitae?</p>
-          <h3>Ruben Barajas</h3>
+          <p>Rubén A. Barajas Ruíz (He/Him) is the Academic Coaching Associate at Highland Park High School. Rubén graduated from Davidson College in 2018 with a BA in Political Science and a minor in Latin American Studies. Rubén likes sports, especially soccer. He really enjoys spending time with his family and friends, and buying Jordans here and there. Rubén's most memorable experience was studying abroad in Peru and visiting so many different parts of the country, including one of the 7 Wonders of the World, Machu Picchu. Some fun facts about Rubén is that he studied Russian for two years in High School. He met Supreme Court Justice Sonia Sotomayor. <br><br> Also, while studying abroad in Peru, he saw Malia Obama at the Peru-Bolivia border. </p>
+          <h3>Rubén A. Barajas Ruíz</h3>
           <span>Staff</span>
           <img src="images/Ruben Barajas_87.jpeg">
           <span><a href="mailto:rubenr@schulerprogram.org" class="mail-link">rubenr@schulerprogram.org</a><br>(773) 526-9080</span>
         </div>
         <div class="swiper-slide slide">
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea atque officiis vero temporibus? Vitae, recusandae tenetur. Vel quaerat maiores dolores rem, obcaecati non, voluptas animi quidem deleniti accusamus nihil doloribus nesciunt. Deleniti, totam commodi ea modi recusandae praesentium assumenda doloribus, impedit amet placeat sunt vero labore asperiores suscipit fugit provident sapiente ipsum beatae accusantium corrupti mollitia enim maxime et cupiditate? Nemo eos laborum asperiores eligendi!</p>
+          <p>Maeve Creagan (She/Her) has been an Associate Scholar Coach at Highland Park for going on four years.  She attended Hobart and William Smith College where she earned a degree in Political Science and another in American History.  After working at the Geneva Boys and Girls Club while leading service groups, Maeve accepted a position with Schuler Program Americorps.  After her two years of service, Maeve became a full Schuler staff member. <br><br> Maeve has a dog named Willa, they spend lots of time outside together exploring all that nature has to offer.</p>
           <h3>Maeve Creagan</h3>
           <span>Staff</span>
           <img src="images/Maeve Creagan.jpeg">
           <span><a href="mailto:maevec@schulerprogram.org" class="mail-link">maevec@schulerprogram.org</a><br>(607) 351-7538</span>
         </div>
         <div class="swiper-slide slide">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum autem dolorem consequuntur, quo nisi reiciendis iusto culpa facilis, recusandae, voluptatibus architecto accusamus totam similique eius ab dolore. Voluptatem necessitatibus cum a praesentium suscipit, mollitia porro, obcaecati illum fugiat quam incidunt rem laboriosam illo qui quae reiciendis numquam exercitationem delectus? Ad ut tempora voluptas sed, unde consectetur totam nihil. Ratione quo, ducimus expedita saepe placeat nemo.</p>
+          <p>Noelle Dela Cruz (She/Her) is the Educational Counselor at Schuler Program at Highland Park High School. She is currently pursuing her Masters of Education in Urban Higher Education. In her free time, she loves to travel and go to concerts. She's excited to continue to help the Scholars achieve academic success. </p>
           <h3>Noelle Dela Cruz</h3>
           <span>Staff</span>
           <img src="images/Noelle Dela Cruz.jpeg">
           <span><a href="mailto:noelle@schulerprogram.org" class="mail-link">noelle@schulerprogram.org</a><br>(224) 814-2357</span>
         </div>
         <div class="swiper-slide slide">
-          <p>Belle (she/her) is a College Counselor at Highland Park. She was a student at HPHS herself, and it has been so fun for her to be back working in the community in which she grew up. Belle graduated from Northern Illinois University where she studied Nonprofit and NGO Studies as well as Political Science and French. <br><br> Belle loves cats, and one of her favorite pre-covid activities was volunteering at her local animal shelter, facilitating adoptions and fundraising.</p>
+          <p>Belle (She/Her) is a College Counselor at Highland Park. She was a student at HPHS herself, and it has been so fun for her to be back working in the community in which she grew up. Belle graduated from Northern Illinois University where she studied Nonprofit and NGO Studies as well as Political Science and French. <br><br> Belle loves cats, and one of her favorite pre-covid activities was volunteering at her local animal shelter, facilitating adoptions and fundraising.</p>
           <h3>Isabella Diaz</h3>
           <span>Staff</span>
           <img src="images/Isabelle Diaz 2.jpeg">

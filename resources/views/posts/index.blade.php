64b432e790e7b023ae9e9013831191e80ae72490
Rook <kristian.velazquez25.schuler@gmail.com>
2023-07-01 18:40:47 -0500

    Instagram

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,7 @@
 <x-layout :title="'Posts'">
    
 <x-heading :file="'header-bg-3'">Posts</x-heading>
-<div class='sk-instagram-feed' data-embed-id='161211'></div><script src='https://widgets.sociablekit.com/instagram-feed/widget.js' async defer></script>
+<div class="tagembed-container" style=" width:100%;height:100%;overflow: auto;"><div class="tagembed-socialwall" data-wall-id="108114"></div> <script src="//widget.tagembed.com/embed.min.js" type="text/javascript"></script></div>
 <!-- <section class="packages">
     <div class="box-container">
         @if ($posts->count())

96a5b66ecd0d3ebaf5b5d07740eeaca400b5b1b7
Rook <kristian.velazquez25.schuler@gmail.com>
2023-07-02 13:33:15 -0500

    Spanish Instagram

--- resources/views/es/posts/index.blade.php
+++ resources/views/es/posts/index.blade.php
@@ -1,18 +1,6 @@
 <x-es.layout :title="'Publicaciones'">
    
 <x-heading :file="'header-bg-3'">Publicaciones</x-heading>
-<section class="packages">
-<div class="box-container">
-        @if ($posts->count())
-            <x-es.posts-grid :posts="$posts" />
-            {{$posts->links()}}
-        @else 
-            <p class="heading-title">Aún no hay publicaciones, vuelve más tarde.</p>
-        @endif
-    </div>
-    @if ($posts->count() > 6)
-      <div class="load-more"> <a href="/es/posts" class="btn">Ver Más</a> </div>
-    @endif
-</section>
+<div class="tagembed-container" style=" width:100%;height:100%;overflow: auto;"><div class="tagembed-socialwall" data-wall-id="108114"></div> <script src="//widget.tagembed.com/embed.min.js" type="text/javascript"></script></div>
 
 </x-es.layout>

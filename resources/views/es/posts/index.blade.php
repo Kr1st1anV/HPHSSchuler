a66e8cabba15d19dda857c75d21ad3c9208ad95b
Rook <kristian.velazquez25.schuler@gmail.com>
2023-07-03 18:09:34 -0500

    Instagram Posts

--- resources/views/es/posts/index.blade.php
+++ resources/views/es/posts/index.blade.php
@@ -1,6 +1,6 @@
 <x-es.layout :title="'Publicaciones'">
    
 <x-heading :file="'header-bg-3'">Publicaciones</x-heading>
-<div class="tagembed-container" style=" width:100%;height:100%;overflow: auto;"><div class="tagembed-socialwall" data-wall-id="108114"></div> <script src="//widget.tagembed.com/embed.min.js" type="text/javascript"></script></div>
+<div class="tagembed-container" style=" width:100%;height:100%;overflow: auto;"><div class="tagembed-socialwall" data-wall-id="108418"></div> <script src="//widget.tagembed.com/embed.min.js" type="text/javascript"></script></div>
 
 </x-es.layout>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout :title="'Posts'">
    
 <x-heading :file="'header-bg-3'">Posts</x-heading>
-<div class="tagembed-container" style=" width:100%;height:100%;overflow: auto;"><div class="tagembed-socialwall" data-wall-id="108114"></div> <script src="//widget.tagembed.com/embed.min.js" type="text/javascript"></script></div>
+<div class="tagembed-container" style=" width:100%;height:100%;overflow: auto;"><div class="tagembed-socialwall" data-wall-id="108418"></div> <script src="//widget.tagembed.com/embed.min.js" type="text/javascript"></script></div>
 </x-layout>
